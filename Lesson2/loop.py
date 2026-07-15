fd67e8b20402547c225713f0c970cdba0b070462
ishalis1990-a11y <isha.lis1990@gmail.com>
2026-07-15 20:49:46 +0530

    Updated Code

---
+++ Lesson2/loop.py
@@ -0,0 +1,14 @@
+for a in range(0,11):
+    print(a)
+
+fruits=["orange","apple","banana"]  
+
+for a in range(0,len(fruits)):
+    print(fruits[a]) 
+
+name="Keerthana"  
+for i in range(0,len(name)):
+    print(name[i])
+
+for i in name:
+    print(i)    
